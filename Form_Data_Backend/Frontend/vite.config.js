cb247b32eb82d74eda78d5dbf925430462ff820a
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2025-11-21 22:10:09 +0530

    form started 2

--- Form_Data_Backend/Frontend/vite.config.js
+++ Form_Data_Backend/Frontend/vite.config.js
@@ -5,5 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	
+	build: {
+		outDir: "build", // Or any other desired name
+	},
 });
