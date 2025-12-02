51f8c008a80ea377dd3870284c29ed0b7bdd5929
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2025-12-02 19:15:29 +0530

    token check for get removed

--- Form_Data_Backend/Backend/src/routes/Formroutes.js
+++ Form_Data_Backend/Backend/src/routes/Formroutes.js
@@ -6,31 +6,31 @@ const FormRouter = express.Router()
 
 // personal info 
 
-FormRouter.get("/personal_info",tokencheck,personal_get)
+FormRouter.get("/personal_info",personal_get)
 
 FormRouter.post("/personal_info",tokencheck,personal_post)
 
 // educational info
 
-FormRouter.get("/educational_info",tokencheck,educational_get)
+FormRouter.get("/educational_info",educational_get)
 
 FormRouter.post("/educational_info",tokencheck,educational_post)
 
 // professional info
 
-FormRouter.get("/professional_info",tokencheck,professional_get)
+FormRouter.get("/professional_info",professional_get)
 
 FormRouter.post("/professional_info",tokencheck,professional_post)
 
 // identity info
 
-FormRouter.get("/identity_info",tokencheck,identity_get)
+FormRouter.get("/identity_info",identity_get)
 
 FormRouter.post("/identity_info",tokencheck,identity_post)
 
 // Review 
 
-FormRouter.get("/review_info",tokencheck,Review_get)
+FormRouter.get("/review_info",Review_get)
 
 FormRouter.post("/review_info",tokencheck,Review_post)
 
